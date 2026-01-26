72bf8cdf5ac153bdfc8f698055634fceffa2b60a
Divya <divyabalagopal2000@gmail.com>
2026-01-26 16:12:23 +0530

    UserRepo bug fixed

--- src/main/java/com/springprojects/banking_application/repository/UserRepo.java
+++ src/main/java/com/springprojects/banking_application/repository/UserRepo.java
@@ -3,7 +3,7 @@ package com.springprojects.banking_application.repository;
 import com.springprojects.banking_application.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface userRepo extends JpaRepository<User, Long> {
+public interface UserRepo extends JpaRepository<User, Long> {
 
     Boolean existsByEmail(String email);
     Boolean existsByAccountNumber(String accountNumber);
