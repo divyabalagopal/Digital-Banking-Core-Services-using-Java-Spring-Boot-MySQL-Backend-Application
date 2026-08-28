4f6919561ea2f270ff6f19426546aef9f32ac24d
Divya Balagopal <divyabalagopal2000@gmail.com>
2026-08-28 08:07:17 +0530

    bug fixed - slf4j logger imported

--- src/main/java/com/springprojects/banking_application/aspect/ServiceLoggingAspect.java
+++ src/main/java/com/springprojects/banking_application/aspect/ServiceLoggingAspect.java
@@ -1,11 +1,11 @@
 package com.springprojects.banking_application.aspect;
 
-import com.itextpdf.text.log.LoggerFactory;
 import org.slf4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
